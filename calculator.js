c297f3986bf971427d08d506866024d9312a439f
parentda <danielparent21@gmail.com>
2021-07-14 18:15:00 -0400

    Added basic math functions

---
+++ calculator.js
@@ -0,0 +1,27 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function power(num1, num2) {
+  return num1 ** num2;
+}
+
+function root(num1, num2) {
+  return num1 ** (1 / num2);
+}
+
+function negate(num) {
+  return -num;
+}
