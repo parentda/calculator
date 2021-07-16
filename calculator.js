0c04782f1e6500eac4f1537e4243fd5a0d8955cf
parentda <danielparent21@gmail.com>
2021-07-16 19:13:38 -0400

    Removed unnecessary function definitions

--- calculator.js
+++ calculator.js
@@ -1,31 +1,3 @@
-function add(num1, num2) {
-  return num1 + num2;
-}
-
-function subtract(num1, num2) {
-  return num1 - num2;
-}
-
-function multiply(num1, num2) {
-  return num1 * num2;
-}
-
-function divide(num1, num2) {
-  return num1 / num2;
-}
-
-function power(num1, num2) {
-  return num1 ** num2;
-}
-
-function root(num1, num2) {
-  return num1 ** (1 / num2);
-}
-
-function negate(num) {
-  return -num;
-}
-
 const operators = {
   "-": {
     type: "operator",
